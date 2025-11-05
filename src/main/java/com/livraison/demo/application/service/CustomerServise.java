615ac1fea2e9b5d2c6566be6864f07a26b48062f
MohamedMoustir <itsmoustir@gmail.com>
2025-11-05 15:25:05 +0100

    Add CustomerService

--- src/main/java/com/livraison/demo/application/service/CustomerServise.java
+++ src/main/java/com/livraison/demo/application/service/CustomerServise.java
@@ -1,4 +1,24 @@
 package com.livraison.demo.application.service;
 
+import com.livraison.demo.domain.dao.CustomerDAO;
+import com.livraison.demo.domain.entity.Customer;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CustomerServise {
+
+    private final CustomerDAO customerDAO ;
+
+    @Autowired
+    public CustomerServise(CustomerDAO customerDAO) {
+        this.customerDAO = customerDAO;
+    }
+
+    public Customer createCustomer( Customer customer){
+        return this.customerDAO.save(customer);
+    }
+
+
 }
