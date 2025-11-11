2dfed00ab4040e2b5e6a0174959867eccbb0712a
MohamedMoustir <itsmoustir@gmail.com>
2025-11-11 21:38:12 +0100

    SDGOTL-56: Add AppConfig for RestTemplate and JSON mapping configuration

--- src/main/java/com/livraison/demo/config/AppConfig.java
+++ src/main/java/com/livraison/demo/config/AppConfig.java
@@ -1,11 +1,15 @@
-import com.google.api.client.util.Value;
+package com.livraison.demo.config;
+
 import com.livraison.demo.application.service.DeepseekClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-@Value("${spring.ai.openai.api-key}")
-private String apiKey;
+@Configuration
+public class AppConfig {
 
-@Bean
-public DeepseekClient deepseekClient() {
-    return new DeepseekClien(apiKey);
+    @Bean
+    public DeepseekClient deepseekClient() {
+        String apiKey = "sk-a4f186bf4719447c8bdbc0c6397a0617";
+        return new DeepseekClient(apiKey);
+    }
 }
