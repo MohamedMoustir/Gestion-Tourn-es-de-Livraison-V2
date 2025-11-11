8a4740dc4c1838a9730b185df3b548b1a2045f25
MohamedMoustir <itsmoustir@gmail.com>
2025-11-11 21:38:11 +0100

    SDGOTL-56: Add DeepseekController to expose AI optimization endpoints

--- src/main/java/com/livraison/demo/presentation/controller/DeepseekController.java
+++ src/main/java/com/livraison/demo/presentation/controller/DeepseekController.java
@@ -1,4 +1,33 @@
 package com.livraison.demo.presentation.controller;
 
-public class deep {
+import com.livraison.demo.application.dto.TourDTO;
+import com.livraison.demo.application.mapper.DeliveryHistoryMapper;
+import com.livraison.demo.application.mapper.TourMapper;
+import com.livraison.demo.application.service.DeepseekClient;
+import com.livraison.demo.application.service.optimizer.AIOptimizer;
+import com.livraison.demo.domain.entity.Tour;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/deepseek")
+public class DeepseekController {
+
+    private final DeepseekClient deepseekClient;
+    private final DeliveryHistoryMapper mapper;
+    private final TourMapper tourMapper;
+    public DeepseekController(DeepseekClient deepseekClient, DeliveryHistoryMapper mapper, TourMapper tourMapper) {
+        this.deepseekClient = deepseekClient;
+        this.mapper = mapper;
+        this.tourMapper = tourMapper;
+    }
+
+
+    @PostMapping("/chat")
+    public List<Map<String, Object>> calculateOptimalTour(@RequestBody TourDTO tourDTO) {
+        Tour tour = tourMapper.toEntity(tourDTO);
+        return deepseekClient.calculateOptimalTour(tour);
+    }
 }
